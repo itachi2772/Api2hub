65d7980538c58e792ffd569c2edba168cdda5a2e
itachi2772 <vivianatunena125@gmail.com>
2025-11-22 16:20:57 +0530

    Update otp.js

--- api/otp.js
+++ api/otp.js
@@ -3,39 +3,10 @@ import https from "https";
 import zlib from "zlib";
 
 export default async function handler(req, res) {
-  const get = (url, headers) =>
-    new Promise((resolve, reject) => {
-      const lib = url.startsWith("https") ? https : http;
-      const request = lib.get(url, { headers }, (response) => {
-        let chunks = [];
-
-        response.on("data", (chunk) => chunks.push(chunk));
-        response.on("end", () => {
-          const buffer = Buffer.concat(chunks);
-          const encoding = response.headers["content-encoding"];
-
-          try {
-            if (encoding === "gzip") {
-              zlib.gunzip(buffer, (err, decoded) => {
-                if (err) return reject(err);
-                resolve(decoded.toString());
-              });
-            } else if (encoding === "deflate") {
-              zlib.inflate(buffer, (err, decoded) => {
-                if (err) return reject(err);
-                resolve(decoded.toString());
-              });
-            } else {
-              resolve(buffer.toString());
-            }
-          } catch (e) {
-            reject(e);
-          }
-        });
-      });
-
-      request.on("error", reject);
-    });
+  const get = (url) =>
+    fetch(url)
+      .then((r) => r.text())
+      .catch((e) => "");
 
   const { searchParams } = new URL(req.url, "http://localhost");
   const type = searchParams.get("type");
@@ -45,31 +16,25 @@ export default async function handler(req, res) {
     return res.end(JSON.stringify({ error: "Missing ?type parameter" }));
   }
 
-  const headers = {
-    "User-Agent":
-      "Mozilla/5.0 (Linux; Android 13; V2040 Build/TP1A.220624.014) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.7444.102 Mobile Safari/537.36",
-    Accept: "application/json, text/javascript, */*; q=0.01",
-    "X-Requested-With": "XMLHttpRequest",
-    "Accept-Encoding": "gzip, deflate",
-    "Accept-Language": "en-US,en;q=0.9",
-    Cookie: "PHPSESSID=r978psgjfgg12iv8bps4bhttml",
-  };
-
-  let url;
+  let targetURL = "";
 
   if (type === "numbers") {
-    url = "http://139.99.63.204/ints/client/res/data_smsnumbers.php";
-    headers.Referer = "http://139.99.63.204/ints/client/MySMSNumbers";
+    targetURL =
+      "http://139.99.63.204/ints/client/res/data_smsnumbers.php";
   } else if (type === "sms") {
-    url = "http://139.99.63.204/ints/client/res/data_smscdr.php";
-    headers.Referer = "http://139.99.63.204/ints/client/SMSCDRStats";
+    targetURL =
+      "http://139.99.63.204/ints/client/res/data_smscdr.php";
   } else {
     res.statusCode = 400;
     return res.end(JSON.stringify({ error: "Invalid type" }));
   }
 
+  // Proxy lagaya
+  const proxyURL =
+    "https://api.allorigins.win/raw?url=" + encodeURIComponent(targetURL);
+
   try {
-    const data = await get(url, headers);
+    const data = await get(proxyURL);
     res.setHeader("Content-Type", "application/json");
     res.end(data);
   } catch (err) {
